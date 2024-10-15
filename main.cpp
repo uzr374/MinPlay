c2d45e915fce80d99ad1e4b2f7d53b3bbb7f3520
uzr374 <uzr374@gmail.com>
2024-10-15 19:22:33 +0300

    Implement dark theme

--- main.cpp
+++ main.cpp
@@ -5,12 +5,33 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QPalette>
+
+static QPalette getDarkPalette(){
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(45, 45, 45));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(30, 30, 30));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(45, 45, 45));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(45, 45, 45));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+
+    darkPalette.setColor(QPalette::Highlight, QColor(100, 100, 150));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    return darkPalette;
+}
 
 int main(int argc, char *argv[])
 {
     const bool sdl_initialized = SDL_Init(0);
     if(!sdl_initialized) return -1;
     QApplication a(argc, argv);
+    a.setPalette(getDarkPalette());
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
